78f8a4723372eac82233923fdf5c4eb1a2591772
xiongxicheng <32041354+xiongxicheng@users.noreply.github.com>
2017-10-23 16:31:42 -0400

    Add list view, enable add button

--- app/src/main/java/com/example/xiongxicheng/wepack/EditListActivity.java
+++ app/src/main/java/com/example/xiongxicheng/wepack/EditListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,20 +19,26 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.*;
-
+import android.widget.AdapterView;
 public class EditListActivity extends AppCompatActivity implements View.OnClickListener{
 
-//    private FirebaseAuth firebaseAuth;
+
 //    private DatabaseReference databaseReference;
 //    private FirebaseDatabase database;
 //    private DataSnapshot dataSnapshot;
-//    private ListView listViewPackList;
-//    @Override
+    private ListView listViewPackList;
+    private ArrayAdapter<String> myAdapter;
+
     private FirebaseAuth firebaseAuth;
     private Button buttonLogout;
     private Button buttonShare;
     private Button buttonSave;
+    private Button buttonAdd;
 
+    private EditText editTextAdd;
+    private EditText editTextShare;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_list);
@@ -43,16 +50,27 @@ public class EditListActivity extends AppCompatActivity implements View.OnClickL
             startActivity(new Intent(this,LoginActivity.class));
         }
         buttonLogout = (Button)findViewById(R.id.buttonLogout);
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
         buttonSave = (Button)findViewById(R.id.buttonSave);
         buttonShare= (Button)findViewById(R.id.buttonShare);
+
+        editTextAdd = (EditText) findViewById(R.id.editTextAdd);
+        editTextShare = (EditText) findViewById(R.id.editTextShare);
+
         buttonLogout.setOnClickListener(this);
         buttonShare.setOnClickListener(this);
         buttonSave.setOnClickListener(this);
+        buttonAdd.setOnClickListener(this);
 
 //  Listview not working
-//        String[] arr = new String[]{"as", "ad", "af"};
-//        listViewPackList = (ListView) findViewById(R.id.listViewPackList);
-//        listViewPackList.setAdapter(new ArrayAdapter<String>(this,R.layout.activity_edit_list, arr));
+        String[] arr = new String[]{"pajama", "raincoat", "flash light"};
+        listViewPackList = (ListView) findViewById(R.id.listViewPackList);
+        listViewPackList.setSelector(R.drawable.listselector);
+        myAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
+        listViewPackList.setAdapter(myAdapter);
+        for(int i=0;i<arr.length;i++){
+            myAdapter.add(arr[i]);
+        }
 
 //        final List<PackList> packLists = new ArrayList<>();
 //
@@ -82,6 +100,10 @@ public class EditListActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View view){
+        if(view == buttonAdd){
+            String item = editTextAdd.getText().toString().trim();
+            myAdapter.add(item);
+        }
         if(view == buttonLogout){
             firebaseAuth.signOut();
             finish();
